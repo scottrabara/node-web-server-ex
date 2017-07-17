46c8fca3f856a7fdf0a6e6208724d444b64ad176
Scott Rabara <scottrabara@gmail.com>
2017-07-16 17:00:05 -0700

    Added heroku compatibility for port and also npm start

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+//Need this for heroku
+const port = process.env.PORT || 3000;
+
+
 var app = express();
 
 //Take directory you wanna use for html partials
@@ -77,6 +81,6 @@ app.get('/bad', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
